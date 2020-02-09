5225a3173b6320cfa7fae132db0c89c0c50fa578
tech-partha <samal.parthasarathi987@gmail.com>
2020-02-09 16:38:08 +0530

    kkkk

---
+++ src/com/fmw/controller/ViewHomeController.java
@@ -0,0 +1,25 @@
+package com.fmw.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+public class ViewHomeController extends AbstractController{
+
+	@Override
+	protected ModelAndView handleRequestInternal(HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+		ModelAndView mav=null;
+		
+		mav=new ModelAndView();
+		mav.setViewName("home");
+		
+		
+		return mav;
+	}
+	
+	
+
+}
